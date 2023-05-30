8a0969e359f334f892e725c1691f28116acb4adb
S-Sadi <shekhsadi100@gmail.com>
2023-05-30 18:54:03 +0600

    Data list

--- src/product/admin.py
+++ src/product/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Product, ProductVariant, ProductVariantPrice, Variant
 # Register your models here.
+admin.site.register(Product)
+admin.site.register(ProductVariant)
+admin.site.register(ProductVariantPrice)
+admin.site.register(Variant)
--- src/product/views/product.py
+++ src/product/views/product.py
@@ -1,6 +1,7 @@
 from django.views import generic
+from django.core.paginator import Paginator
 
-from product.models import Variant
+from product.models import Variant, Product, ProductVariantPrice
 
 
 class CreateProductView(generic.TemplateView):
@@ -10,5 +11,18 @@ class CreateProductView(generic.TemplateView):
         context = super(CreateProductView, self).get_context_data(**kwargs)
         variants = Variant.objects.filter(active=True).values('id', 'title')
         context['product'] = True
-        context['variants'] = list(variants.all())
+        context['variants'] = list( variants.all() )
+        return context
+
+class ProductListView(generic.TemplateView):
+    template_name = "products/list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        all_products = Product.objects.all()
+        paginator = Paginator(all_products, per_page=3)
+        page_num = self.request.GET.get('page')
+        pages = paginator.get_page(page_num)
+        context["pages"] = pages
+        context["total_products"] = paginator.count
         return context
